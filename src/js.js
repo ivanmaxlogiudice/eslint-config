70d333dd105d98037d2e9f0a39ef8d6075cb4463
Iván Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-09-02 21:48:25 -0300

    feat: replace standard for local config

--- src/js.js
+++ src/js.js
@@ -1,8 +1,8 @@
 import globals from 'globals'
 import jsConfig from '@eslint/js'
-import standardConfig from 'eslint-config-standard'
 import nPlugin from 'eslint-plugin-n'
 import importPlugin from 'eslint-plugin-import'
+import promisePlugin from 'eslint-plugin-promise'
 import unusedImportsPlugin from 'eslint-plugin-unused-imports'
 import unicornPlugin from 'eslint-plugin-unicorn'
 import antfuPlugin from 'eslint-plugin-antfu'
@@ -24,30 +24,41 @@ export const js = [
             sourceType: 'module',
         },
         plugins: {
-            standard: standardConfig,
             n: nPlugin,
+            promise: promisePlugin,
         },
         rules: {
-            // ...standardConfig,
-            
+            'accessor-pairs': ['error', {
+                setWithoutGet: true,
+                enforceForClassMembers: true,
+            }],
             'array-bracket-spacing': ['error', 'never'],
             'array-callback-return': 'error',
             'arrow-parens': ['error', 'as-needed', {
                 requireForBlockBody: true,
+            }],
+            'arrow-spacing': ['error', {
+                before: true,
+                after: true,
             }],
             'block-scoped-var': 'error',
             'block-spacing': ['error', 'always'],
             'brace-style': ['error', 'stroustrup', {
                 allowSingleLine: true,
             }],
-            'consistent-return': 'error',
             'comma-dangle': ['error', 'always-multiline'],
             'comma-spacing': ['error', {
                 after: true,
                 before: false,
             }],
             'comma-style': ['error', 'last'],
+            'computed-property-spacing': ['error', 'never', {
+                enforceForClassMembers: true,
+            }],
+            'consistent-return': 'error',
             'curly': ['error', 'multi-or-nest', 'consistent'],
+            'default-case-last': 'error',
+            'dot-location': ['error', 'property'],
             'dot-notation': 'warn',
             'eqeqeq': ['error', 'smart'],
             'func-call-spacing': ['error', 'never'],
@@ -60,15 +71,44 @@ export const js = [
                 afterColon: true,
                 beforeColon: false,
             }],
+            'keyword-spacing': ['error', {
+                before: true,
+                after: true,
+            }],
+            'lines-between-class-members': ['error', 'always', {
+                exceptAfterSingleLine: true,
+            }],
             'max-statements-per-line': ['error', {
                 max: 1,
             }],
+            'multiline-ternary': ['error', 'always-multiline'],
+            'new-cap': ['error', {
+                newIsCap: true,
+                capIsNew: false,
+                properties: true,
+            }],
+            'new-parens': 'error',
+            'object-curly-newline': ['error', {
+                multiline: true,
+                consistent: true,
+            }],
             'object-curly-spacing': ['error', 'always'],
+            'object-property-newline': ['error', {
+                allowMultiplePropertiesPerLine: true,
+            }],
+            'one-var': ['error', {
+                initialized: 'never',
+            }],
             'object-shorthand': ['error', 'always', {
                 avoidQuotes: true,
                 ignoreConstructors: false,
             }],
             'operator-linebreak': ['error', 'before'],
+            'padded-blocks': ['error', {
+                blocks: 'never',
+                switches: 'never',
+                classes: 'never',
+            }],
             'prefer-arrow-callback': ['error', {
                 allowNamedFunctions: false,
                 allowUnboundThis: true,
@@ -78,6 +118,10 @@ export const js = [
                 ignoreReadBeforeAssign: true,
             }],
             'prefer-exponentiation-operator': 'error',
+            'prefer-promise-reject-errors': 'error',
+            'prefer-regex-literals': ['error', {
+                disallowRedundantWrapping: true,
+            }],
             'prefer-rest-params': 'error',
             'prefer-spread': 'error',
             'prefer-template': 'error',
@@ -85,7 +129,12 @@ export const js = [
             'quotes': ['error', 'single', {
                 avoidEscape: true,
             }],
+            'rest-spread-spacing': ['error', 'never'],
             'semi': ['error', 'never'],
+            'semi-spacing': ['error', {
+                before: false,
+                after: true,
+            }],
             'sort-imports': ['error', {
                 allowSeparatedGroups: false,
                 ignoreCase: false,
@@ -99,6 +148,12 @@ export const js = [
                 asyncArrow: 'always',
                 named: 'never',
             }],
+            'space-in-parens': ['error', 'never'],
+            'space-infix-ops': 'error',
+            'space-unary-ops': ['error', {
+                words: true,
+                nonwords: false,
+            }],
             'spaced-comment': ['error', 'always', {
                 block: {
                     balanced: true,
@@ -110,28 +165,106 @@ export const js = [
                     markers: ['/'],
                 },
             }],
-            'require-await': 'error',
+            'symbol-description': 'error',
             'template-curly-spacing': 'error',
+            'template-tag-spacing': ['error', 'never'],
+            'unicode-bom': ['error', 'never'],
+            'use-isnan': ['error', {
+                enforceForSwitchCase: true,
+                enforceForIndexOf: true,
+            }],
+            'valid-typeof': ['error', {
+                requireStringLiterals: true,
+            }],
+            'wrap-iife': ['error', 'any', {
+                functionPrototypeMethods: true,
+            }],
+            'yield-star-spacing': ['error', 'both'],
+            'yoda': ['error', 'never'],
+            'require-await': 'error',
             'vars-on-top': 'error',
 
             // no-
             'no-alert': 'warn',
-            'no-cond-assign': ['error', 'always'],
+            'no-array-constructor': 'error',
+            'no-async-promise-executor': 'error',
+            'no-caller': 'error',
+            'no-case-declarations': 'error',
+            'no-class-assign': 'error',
+            'no-compare-neg-zero': 'error',
+            'no-const-assign': 'error',
             'no-console': ['error', {
                 allow: ['warn', 'error'],
             }],
             'no-constant-condition': 'warn',
+            'no-control-regex': 'error',
             'no-debugger': 'warn',
+            'no-delete-var': 'error',
+            'no-dupe-args': 'error',
+            'no-dupe-class-members': 'error',
+            'no-dupe-keys': 'error',
+            'no-duplicate-case': 'error',
             'no-duplicate-imports': 'error',
-            'no-empty': ['error', { 
-                allowEmptyCatch: true, 
+            'no-empty': ['error', {
+                allowEmptyCatch: true,
             }],
+            'no-empty-character-class': 'error',
+            'no-empty-pattern': 'error',
+            'no-eval': 'error',
+            'no-ex-assign': 'error',
+            'no-extra-bind': 'error',
+            'no-extra-boolean-cast': 'error',
+            'no-extra-parens': ['error', 'functions'],
+            'no-fallthrough': 'error',
+            'no-floating-decimal': 'error',
+            'no-func-assign': 'error',
+            'no-global-assign': 'error',
+            'no-implied-eval': 'error',
+            'no-import-assign': 'error',
+            'no-invalid-regexp': 'error',
             'no-invalid-this': 'error',
+            'no-irregular-whitespace': 'error',
+            'no-iterator': 'error',
+            'no-labels': ['error', {
+                allowLoop: false,
+                allowSwitch: false,
+            }],
+            'no-lone-blocks': 'error',
             'no-lonely-if': 'error',
+            'no-loss-of-precision': 'error',
+            'no-misleading-character-class': 'error',
+            'no-mixed-operators': ['error', {
+                groups: [
+                    ['==', '!=', '===', '!==', '>', '>=', '<', '<='],
+                    ['&&', '||'],
+                    ['in', 'instanceof'],
+                ],
+                allowSamePrecedence: true,
+            }],
+            'no-mixed-spaces-and-tabs': 'error',
             'no-multi-spaces': 'error',
             'no-multi-str': 'error',
-            'no-restricted-globals': ['error', 
-                { name: 'global', message: 'Use `globalThis` instead.' }, 
+            'no-multiple-empty-lines': ['error', {
+                max: 1,
+                maxBOF: 0,
+                maxEOF: 0,
+            }],
+            'no-new': 'error',
+            'no-new-func': 'error',
+            'no-new-object': 'error',
+            'no-new-symbol': 'error',
+            'no-new-wrappers': 'error',
+            'no-obj-calls': 'error',
+            'no-octal': 'error',
+            'no-octal-escape': 'error',
+            'no-param-reassign': 'error',
+            'no-proto': 'error',
+            'no-redeclare': ['error', {
+                builtinGlobals: false,
+            }],
+            'no-regex-spaces': 'error',
+            'no-restricted-globals': ['error',
+                { name: 'global', message: 'Use `globalThis` instead.' },
                 { name: 'self', message: 'Use `globalThis` instead.' },
             ],
             'no-restricted-properties': ['error',
@@ -146,24 +279,64 @@ export const js = [
                 'LabeledStatement',
                 'WithStatement',
             ],
-            'no-param-reassign': 'error',
+            'no-return-assign': ['error', 'except-parens'],
+            'no-self-assign': ['error', {
+                props: true,
+            }],
+            'no-self-compare': 'error',
+            'no-sequences': 'error',
+            'no-shadow-restricted-names': 'error',
+            'no-sparse-arrays': 'error',
+            'no-tabs': 'error',
+            'no-template-curly-in-string': 'error',
+            'no-this-before-super': 'error',
+            'no-throw-literal': 'error',
+            'no-trailing-spaces': 'error',
+            'no-undef': 'error',
+            'no-undef-init': 'error',
+            'no-unexpected-multiline': 'error',
+            'no-unmodified-loop-condition': 'error',
+            'no-unneeded-ternary': ['error', {
+                defaultAssignment: false,
+            }],
+            'no-unreachable': 'error',
+            'no-unreachable-loop': 'error',
+            'no-unsafe-finally': 'error',
+            'no-unsafe-negation': 'error',
             'no-unused-expressions': ['error', {
                 allowShortCircuit: true,
                 allowTernary: true,
                 allowTaggedTemplates: true,
             }],
-            'no-useless-escape': 'error',
             'no-use-before-define': ['error', {
                 classes: false,
                 functions: false,
                 variables: true,
             }],
+            'no-useless-backreference': 'error',
+            'no-useless-call': 'error',
+            'no-useless-computed-key': 'error',
+            'no-useless-constructor': 'error',
+            'no-useless-escape': 'error',
+            'no-useless-rename': 'error',
+            'no-useless-return': 'error',
             'no-var': 'error',
             'no-void': 'error',
+            'no-whitespace-before-property': 'error',
+            'no-with': 'error',
 
             // node
-            'n/prefer-global/buffer': ['error', 'never'],
+            'n/handle-callback-err': ['error', '^(err|error)$'],
             'n/no-callback-literal': 'off',
+            'n/no-deprecated-api': 'error',
+            'n/no-exports-assign': 'error',
+            'n/no-new-require': 'error',
+            'n/no-path-concat': 'error',
+            'n/prefer-global/buffer': ['error', 'never'],
+            'n/process-exit-as-throw': 'error',
+
+            // promises
+            'promise/param-names': 'error',
 
             // off
             'camelcase': 'off',
@@ -218,6 +391,7 @@ export const imports = [
             },
         },
         rules: {
+            'import/export': 'error',
             'import/first': 'error',
             'import/newline-after-import': ['error', {
                 considerComments: true,
@@ -226,6 +400,9 @@ export const imports = [
             'import/no-default-export': 'error',
             'import/no-duplicates': 'error',
             'import/no-mutable-exports': 'error',
+            'import/no-named-default': 'error',
+            'import/no-self-import': 'error',
+            'import/no-webpack-loader-syntax': 'error',
             'import/order': ['error', {
                 groups: [
                     'builtin',
@@ -243,7 +420,6 @@ export const imports = [
                 }],
                 pathGroupsExcludedImportTypes: ['type'],
             }],
-            'import/no-self-import': 'error',
 
             // off
             'import/namespace': 'off',
@@ -260,7 +436,7 @@ export const imports = [
                 args: 'after-used',
                 argsIgnorePattern: '^_',
             }],
-            
+
             'antfu/no-import-node-modules-by-path': 'error',
             'antfu/if-newline': 'error',
             'antfu/import-dedupe': 'error',
@@ -299,10 +475,10 @@ export const unicorn = [
             'unicorn/error-message': 'error',
             'unicorn/escape-case': 'error',
             'unicorn/explicit-length-check': 'error',
-            'unicorn/filename-case': ['error', { 
-                cases: { 
-                    kebabCase: true, 
-                    pascalCase: true, 
+            'unicorn/filename-case': ['error', {
+                cases: {
+                    kebabCase: true,
+                    pascalCase: true,
                 },
                 ignore: [/^[A-Z]+\..*$/],
             }],
@@ -361,4 +537,3 @@ export const unicorn = [
         },
     },
 ]
-  
