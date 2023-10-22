825d4c141f3aa8fef5b9384801a6242663e32f47
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2023-10-22 17:24:43 -0300

    fix: update jsonc rules

--- src/configs/jsonc.ts
+++ src/configs/jsonc.ts
@@ -26,9 +26,32 @@ export function jsonc(options: OptionsStylistic & OptionsOverrides = {}): Config
             },
             name: 'config:jsonc:rules',
             rules: {
-                ...(pluginJsonc.configs['recommended-with-jsonc'].rules as Rules),
-
+                'jsonc/no-bigint-literals': 'error',
+                'jsonc/no-binary-expression': 'error',
+                'jsonc/no-binary-numeric-literals': 'error',
+                'jsonc/no-dupe-keys': 'error',
+                'jsonc/no-escape-sequence-in-identifier': 'error',
+                'jsonc/no-floating-decimal': 'error',
+                'jsonc/no-hexadecimal-numeric-literals': 'error',
+                'jsonc/no-infinity': 'error',
+                'jsonc/no-multi-str': 'error',
+                'jsonc/no-nan': 'error',
+                'jsonc/no-number-props': 'error',
+                'jsonc/no-numeric-separators': 'error',
+                'jsonc/no-octal': 'error',
                 'jsonc/no-octal-escape': 'error',
+                'jsonc/no-octal-numeric-literals': 'error',
+                'jsonc/no-parenthesized': 'error',
+                'jsonc/no-plus-sign': 'error',
+                'jsonc/no-regexp-literals': 'error',
+                'jsonc/no-sparse-arrays': 'error',
+                'jsonc/no-template-literals': 'error',
+                'jsonc/no-undefined-value': 'error',
+                'jsonc/no-unicode-codepoint-escapes': 'error',
+                'jsonc/no-useless-escape': 'error',
+                'jsonc/space-unary-ops': 'error',
+                'jsonc/valid-json-number': 'error',
+                'jsonc/vue-custom-block/no-parsing-error': 'error',
 
                 ...stylistic
                     ? {
@@ -48,8 +71,8 @@ export function jsonc(options: OptionsStylistic & OptionsOverrides = {}): Config
                             'jsonc/object-property-newline': ['error', {
                                 allowMultiplePropertiesPerLine: true,
                             }],
-                            'jsonc/quote-props': 'off',
-                            'jsonc/quotes': 'off',
+                            'jsonc/quote-props': 'error',
+                            'jsonc/quotes': 'error',
                         }
                     : {},
 
