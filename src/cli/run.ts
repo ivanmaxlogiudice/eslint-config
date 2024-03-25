b32a1eda514c2b7fd55f78562570f9be54c82b1d
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2024-03-25 18:30:06 -0300

    fix(cli): optional framework

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -70,6 +70,7 @@ export async function run(options: CliRunOptions = {}) {
                 return p.multiselect<PromItem<FrameworkOption>[], FrameworkOption>({
                     message: c.reset(message),
                     options: frameworkOptions,
+                    required: false,
                 })
             },
             extra: ({ results }) => {
