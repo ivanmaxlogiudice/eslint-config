41d3912ba6323f692ea0e8cf299313a9fde71449
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2024-07-30 16:00:49 -0300

    refactor

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,22 @@
 import config from './src'
 
-export default config(
-    {
-        vue: true,
-        react: true,
-        solid: true,
-        svelte: true,
-        astro: true,
-        typescript: true,
-        formatters: true,
-        type: 'lib',
+export default config({
+    vue: true,
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+    type: 'lib',
+}, {
+    ignores: [
+        'fixtures',
+        '_fixtures',
+    ],
+}, {
+    files: ['src/**/*.ts'],
+    rules: {
+        'perfectionist/sort-objects': 'error',
     },
-    {
-        ignores: [
-            'fixtures',
-            '_fixtures',
-        ],
-    },
-    {
-        files: ['src/**/*.ts'],
-        rules: {
-            'perfectionist/sort-objects': 'error',
-        },
-    },
-)
+})
