2557775b3c558f257ae6087f93f9e8b35a084db1
Iván Máximiliano, Lo Giudice <ivanmaxlogiudice@gmail.com>
2025-09-09 09:10:26 -0300

    fix: enhance TypedFlatConfigItem type

--- src/types.ts
+++ src/types.ts
@@ -4,16 +4,27 @@ import type { RuleOptions } from './typegen'
 
 export type Awaitable<T> = T | Promise<T>
 
-export interface Rules extends RuleOptions {}
+export type Rules = Record<string, Linter.RuleEntry<any> | undefined> & RuleOptions
 
-export type TypedFlatConfigItem = Omit<Linter.Config<Linter.RulesRecord & Rules>, 'plugins'> & {
-    // Relax plugins type limitation, as most of the plugins did not have correct type info yet.
+/**
+ * An updated version of ESLint's `Linter.Config`, which provides autocompletion
+ * for `rules` and relaxes type limitations for `plugins` and `rules`, because
+ * many plugins still lack proper type definitions.
+ */
+export type TypedFlatConfigItem = Omit<Linter.Config, 'plugins' | 'rules'> & {
     /**
-     * An object containing a name-value mapping of plugin names to plugin objects. When `files` is specified, these plugins are only available to the matching files.
+     * An object containing a name-value mapping of plugin names to plugin objects.
+     * When `files` is specified, these plugins are only available to the matching files.
      *
      * @see [Using plugins in your configuration](https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new#using-plugins-in-your-configuration)
      */
     plugins?: Record<string, any>
+
+    /**
+     * An object containing the configured rules. When `files` or `ignores` are
+     * specified, these rule configurations are only available to the matching files.
+     */
+    rules?: Rules
 }
 
 export interface OptionsFiles {
@@ -23,9 +34,9 @@ export interface OptionsFiles {
     files?: string[]
 }
 
-export type OptionsTypescript =
-  (OptionsTypeScriptWithTypes & OptionsOverrides)
-  | (OptionsTypeScriptParserOptions & OptionsOverrides)
+export type OptionsTypescript
+    = (OptionsTypeScriptWithTypes & OptionsOverrides)
+        | (OptionsTypeScriptParserOptions & OptionsOverrides)
 
 export interface OptionsComponentExts {
     /**
